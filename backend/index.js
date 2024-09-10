e5bd606827490cac65f9c39885a36fe774dd0e68
yohannes4321 <alemuyohannes960@gmail.com>
2024-09-10 21:59:12 +0300

    mern

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 8080;
 
 const __dirname1= path.resolve();
 if (process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, 'frotend/build')));
+    app.use(express.static(path.join(__dirname, '/frotend/build')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'frotend/build', 'index.html'));
