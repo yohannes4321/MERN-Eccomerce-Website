7b707b4cba43ca182a65fb357ee8d4c7601497cf
yohannes4321 <alemuyohannes960@gmail.com>
2024-09-14 11:06:10 +0300

    d

--- backend/index.js
+++ backend/index.js
@@ -19,20 +19,6 @@ app.use('/api', router);
 const PORT = process.env.PORT || 8080;
 
 // Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-  app.get('*', (req, res, next) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'), (err) => {
-      if (err) {
-        next(err);
-      }
-    });
-  });
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running successfully');
-  });
-}
 
 // Connect to the database and start the server
 connectDB().then(() => {
