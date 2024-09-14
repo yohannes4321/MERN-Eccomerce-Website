e14131cb04fcb6824da6be025e69d4e42ee924e6
Yohannes Alemu <138892760+yohannes4321@users.noreply.github.com>
2024-09-14 10:12:21 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,53 +4,42 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const router = require('./routes/routes');
-const path = require("path");
+const path = require('path');
+
 const app = express();
 
-// Allowed origins for CORS
-const allowedOrigins = ['https://migibee.onrender.com'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, // Allow credentials (cookies, headers, etc.)
-}));
-
-app.use(cookieParser());
+// Middleware
+app.use(cookieParser()); // Removed the incorrect argument 'credentials: true'
 app.use(express.json()); // For parsing JSON request bodies
+app.use(cors({ credentials: true })); // Use CORS with credentials enabled if needed
 
 // Routes
-app.use("/api", router);
+app.use('/api', router);
 
 const PORT = process.env.PORT || 8080;
 
 // Serve static files in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res, next) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"), (err) => {
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'), (err) => {
       if (err) {
         next(err);
       }
     });
   });
 } else {
-  app.get("/", (req, res) => {
-    res.send("API is running successfully");
+  app.get('/', (req, res) => {
+    res.send('API is running successfully');
   });
 }
 
 // Connect to the database and start the server
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("Connected to the database");
+    console.log('Connected to the database');
     console.log(`Server is running on port ${PORT}`);
   });
 }).catch((error) => {
-  console.error("Failed to connect to the database", error);
+  console.error('Failed to connect to the database', error);
 });
